ba70be25586b3ec9380a6c9dc762e65931f05e8f
Liudmila <liudmilagubanova18@gmail.com>
2023-05-06 19:55:23 +0400

    add changes

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,7 +6,7 @@ use Symfony\Component\Yaml\Yaml;
 
 function parseFile(string $filePath)
 {
-    $fileContent = file_get_contents($filePath);
+    $fileContent = file_get_contents($filePath, false, null, 0);
 
     if (pathinfo($filePath, PATHINFO_EXTENSION) === 'json') {
         $decodedFile = json_decode($fileContent, true);
