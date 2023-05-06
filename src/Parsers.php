1b7b7e252acd967182680b3eabc0d9345aef108a
Liudmila <liudmilagubanova18@gmail.com>
2023-05-06 20:03:54 +0400

    add changes

--- src/Parsers.php
+++ src/Parsers.php
@@ -8,10 +8,12 @@ function parseFile(string $filePath)
 {
     $fileContent = file_get_contents($filePath, false, null, 0);
 
-    if (pathinfo($filePath, PATHINFO_EXTENSION) === 'json') {
-        $decodedFile = json_decode($fileContent, true);
-    } else {
-        $decodedFile = Yaml::parse($fileContent); ////Yaml::PARSE_OBJECT_FOR_MAP
-    }
+    if ($fileContent !== false) {
+        if (pathinfo($filePath, PATHINFO_EXTENSION) === 'json') {
+            $decodedFile = json_decode($fileContent, true);
+        } else {
+        $decodedFile = Yaml::parse($fileContent);
+        }
     return $decodedFile;
+    }
 }
